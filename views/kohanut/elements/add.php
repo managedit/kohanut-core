e7fba911281b84a69e40bfabffa037d4350054e3
Kiall Mac Innes <kiall@managedit.ie>
2010-08-15 07:15:45 +0100

    Removed an extra </div> tag

--- views/kohanut/elements/add.php
+++ views/kohanut/elements/add.php
@@ -5,22 +5,19 @@
 		<?php include Kohana::find_file('views', 'kohanut/errors') ?>
 		
 		<form method="post">
-		
+
 			<?php foreach ($element->inputs() as $label => $input): ?>
 			<p>
 				<label><?php echo __($label) ?></label>
 				<?php echo $input ?>
 			</p>
 			<?php endforeach ?>
-			
+
 			<p>
 				<?php echo Form::submit('submit',__('Add Element'),array('class'=>'submit')) ?>
 				<?php echo html::anchor(Route::get('kohanut-admin')->uri(array('controller'=>'pages','action'=>'edit','params'=>$page)),__('cancel')); ?>
 			</p>
-			
-		</form>
-		
-		</div>
-	</div>
 
+		</form>
+	</div>
 </div>
